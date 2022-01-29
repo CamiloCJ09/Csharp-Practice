7748e7dec603fb49a0975efc28a0ae4c34ca4b6f
CamiloCJ09 <camilocampazj@gmail.com>
2022-01-29 17:45:09 -0500

    Adding file chooser

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Microsoft.VisualBasic.FileIO;
+using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +26,65 @@ namespace WpfApp1
         public MainWindow()
         {
             InitializeComponent();
+            /*
+            using TextFieldParser parser = new TextFieldParser(@"c:\temp\test.csv");
+            parser.TextFieldType = FieldType.Delimited;
+            parser.SetDelimiters(",");
+            while (!parser.EndOfData)
+            {
+                //Processing row
+                string[] fields = parser.ReadFields();
+                foreach (string field in fields)
+                {
+                    //TODO: Process field
+                }
+            }
+            */
         }
+
+        void OnClick(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine("Working");
+            //MessageBox.Show("Hey");
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.InitialDirectory = "c:\\";
+            openFileDialog1.Filter = "Database files (*.csv)|*.csv";
+            openFileDialog1.FilterIndex = 0;
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == true)
+            {
+                string selectedFileName = openFileDialog1.FileName;
+                MessageBox.Show(selectedFileName);
+                System.Diagnostics.Debug.WriteLine(selectedFileName);
+                Console.WriteLine(selectedFileName);
+                Console.WriteLine("Comentarioooooooooooooooooooooooo");
+                System.Diagnostics.Debug.WriteLine("Comentarioooooooooo");
+
+                using (var reader = new StreamReader(selectedFileName))
+                {
+                    List<string> listA = new List<string>();
+                    List<string> listB = new List<string>();
+                    while (!reader.EndOfStream)
+                    {
+                        var line = reader.ReadLine();
+                        var values = line.Split(';');
+
+                        listA.Add(values[0]);
+                        listB.Add(values[1]);
+                    }
+                }
+                //...
+            }
+            else
+            {
+                MessageBox.Show("Ingrese un archivo valido");
+            }
+            //Console.WriteLine("2Comentarioooooooooooooooooooooooo");
+            System.Diagnostics.Debug.WriteLine("2Comentarioooooooooo");
+        }
+
+        
     }
 }
